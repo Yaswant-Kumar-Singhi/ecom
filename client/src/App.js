b944ce7a10e0ae09aa5c6e856d933401cde2aee0
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-12-16 19:28:00 +0530

    added logout feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import {Switch,Route} from 'react-router-dom';
 
 
@@ -11,23 +11,44 @@ import RegisterComplete from './Pages/Auth/RegisterComplete'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+
+
 const App = () => {
-return (
-//since we need to wrap header and switch we can wrap it using fragments. think fragments as div
-<>
-    <Header/>
-    <ToastContainer />
-    <Switch>
-        <Route	exact path="/" component = {Home} />
-        <Route	exact path="/login" component = {Login} />
-        <Route	exact path="/register" component = {Register } />   
-        <Route	exact path="/register/complete" component = {RegisterComplete } />   
-    </Switch>
+  const dispatch = useDispatch();
 
-</>
+  // to check firebase auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+        console.log("user", user);
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+    // cleanup
+    return () => unsubscribe();
+  }, []);
 
-);
+  return (
+    <>
+      <Header />
+      <ToastContainer />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/register/complete" component={RegisterComplete} />
+      </Switch>
+    </>
+  );
 };
-
 
 export default App;
--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,8 +1,10 @@
 import React,{useState} from 'react';
 import { Menu } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined, UserAddOutlined, UserOutlined} from '@ant-design/icons';
+import { MailOutlined, AppstoreOutlined, SettingOutlined, UserAddOutlined, UserOutlined , LogoutOutlined} from '@ant-design/icons';
 import { Link } from 'react-router-dom'
-
+import firebase from 'firebase'
+import {useDispatch} from 'react-redux'
+import {useHistory} from 'react-router-dom'
 
 
 const { SubMenu , Item } = Menu; //Menu.submenu
@@ -10,9 +12,20 @@ const { SubMenu , Item } = Menu; //Menu.submenu
 const Header = () => {
     //creating state and function to update the state
     const [current, setCurrent] = useState('home');
+    let dispatch = useDispatch()
+    let history = useHistory()
 
     const handleClick = (e) =>{
         setCurrent(e.key)
+    }
+
+    const logout = () => {
+        firebase.auth().signOut();
+        dispatch({
+            type : "LOGOUT",
+            payload : null
+        })
+        history.push("/login")
     }
 
     return(
@@ -37,6 +50,7 @@ const Header = () => {
             <SubMenu key="user" icon={<SettingOutlined />} title="Usernmae">
                     <Item key="setting:1">Login</Item>
                     <Item key="setting:2">Register 2</Item>
+                    <Item icon={<LogoutOutlined />} onClick={logout}>Logout</Item>
           </SubMenu>
         
       </Menu> 
